534c198b152094fac2ab6de0590f54723b48820f
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-04-29 02:57:36 -0700

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -33,23 +33,19 @@ plugin_category = "utils"
     },
 )
 async def amireallyalive(event):
-    "A kind of showing bot details"
     reply_to_id = await reply_id(event)
-    uptime = await get_readable_time((time.time() - StartTime))
-    _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
-    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = ""
-    if CAT_IMG:
-        CAT = [x for x in CAT_IMG.split()]
-        A_IMG = list(CAT)
-        PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين سورس 𝐇𝐞𝐥𝐥𝐚𝐬\n"
-        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- المطور  : @F_Q_1\n"
-        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        await event.client.send_file(
-            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+    post_link = "https://t.me/hellassvs/154"  # رابط المنشور
+    message = await l313l.get_messages(entity="imain3", ids=1985)
+    
+    if message and message.media:
+        cat_caption = "ᴏᴡɴᴇʀ ᴏꜰ sᴏᴜʀᴄᴇ ʀᴏʙɪɴ\n"
+        cat_caption += " ╭─━━━━━━𖤐━━━━━━─╮\n"
+        cat_caption += "ᴏᴡɴᴇʀ  : @F_Q_1\n"
+        cat_caption += "BOT  : @qvxbot\n"
+        cat_caption += "CH  : @HELLASUserBot\n"
+        cat_caption += "╰─━━━━━━𖤐━━━━━━─╯\n"
+        await l313l.send_file(
+            event.chat_id, message.media, caption=cat_caption, reply_to=reply_to_id
         )
 
 @l313l.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
@@ -57,7 +53,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 1490479382, 5564802580]
+progs = [1490479382]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
@@ -66,10 +62,8 @@ async def reda(event):
        owner_id = reply_msg.from_id.user_id
        if owner_id == l313l.uid:
            if event.message.message == "حظر من السورس":
-               await event.reply("**حاظر مطوري ، لقد تم حظره من استخدام السورس**")
+               await event.reply("حاظر مطوري ، لقد تم حظره من استخدام السورس")
                addgvar("blockedfrom", "yes")
            elif event.message.message == "الغاء الحظر من السورس":
-               await event.reply("**حاظر مطوري، لقد الغيت الحظر**")
+               await event.reply("حاظر مطوري، لقد الغيت الحظر")
                delgvar("blockedfrom")
-                
-
