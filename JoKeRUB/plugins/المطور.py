081b51640b9dc5b29399267c70f9138003dcf82a
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-05-01 02:23:38 -0700

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -4,8 +4,8 @@ from ..core.managers import edit_or_reply
 plugin_category = "البحث"
 
 @l313l.ar_cmd(
-    pattern="المطور$",
-    command=("المطور", plugin_category),
+    pattern="(مطور|المطور)$",
+    command=("مطور", plugin_category),
     info={
         "header": "لعرض معلومات المطور",
         "الاستـخـدام": "{tr}المطور - لعرض معلومات مطور البوت",
