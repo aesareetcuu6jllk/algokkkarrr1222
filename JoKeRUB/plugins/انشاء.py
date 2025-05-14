9734c7c17b2814472ba196adc99694c57026af67
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-05-14 05:27:37 -0700

    Update انشاء.py

--- JoKeRUB/plugins/انشاء.py
+++ JoKeRUB/plugins/انشاء.py
@@ -1,17 +1,20 @@
+import random
 from JoKeRUB import l313l
 from ..helpers.utils import reply_id
 
 @l313l.on((pattern="زيج$"))
 async def _(event):
     try:
-        rpl = await reply_id(event)
-        url = "https://t.me/SEFHELLAS/312"  # رابط البصمة
+        rl = random.randint(1, 31)
+        url = f"https://t.me/SEFHELLAS/312"
         await event.client.send_file(
             event.chat_id,
             url,
-            caption="",
-            reply_to=rpl
+            caption="⎉╎ عظم الله لنا ولكم الاجر بهذا المُصاب الجلل 🏴",
+            parse_mode="html",
+            reply_to=await reply_id(event)
         )
         await event.delete()
     except Exception as e:
-        await event.reply(f"خطأ: {e}")
+        await event.reply(f"🚫 حدث خطأ:\n{e}")
+
