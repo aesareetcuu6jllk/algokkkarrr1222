f44fb328b3f8e04d828a36ee47cdad7af314766c
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-05-24 15:13:54 +0300

    Update ميمز.py

--- JoKeRUB/plugins/ميمز.py
+++ JoKeRUB/plugins/ميمز.py
@@ -35,6 +35,14 @@ async def jepmeme(memejep):
   url = f"https://t.me/NC2CN/7"
   await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
   await memejep.delete()
+
+@l313l.on(admin_cmd(outgoing=True, pattern="ها يوسف$"))
+async def jepmeme(memejep):
+  Jep = await reply_id(memejep)
+  url = f"https://t.me/AJSJ36/2430"
+  await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
+  await memejep.delete()
+  
 @l313l.on(admin_cmd(outgoing=True, pattern="انا ماشي$"))
 async def jepmeme(memejep):
   Jep = await reply_id(memejep)
