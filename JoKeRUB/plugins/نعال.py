c9e8459dd3f7a6bd03e370839298f8b962b4624d
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-11 04:37:30 -0700

    Create نعال.py

---
+++ JoKeRUB/plugins/نعال.py
@@ -0,0 +1,13 @@
+from telethon import events
+from JoKeRUB import l313l  # غيّرها إذا اسم الكلاينت مختلف
+
+# هنا تحط الإيموجي أو السمايل اللي تريده
+NAAL_EMOJI = "👞"  # مثلا نعال
+
+@l313l.on(events.NewMessage(pattern=r"^\.تعال$"))
+async def naal_smile(event):
+    try:
+        await event.delete()  # حذف رسالتك
+        await event.client.send_message(event.chat_id, NAAL_EMOJI)  # إرسال السمايل
+    except Exception as e:
+        print("خطأ أثناء إرسال النعال:", e)
