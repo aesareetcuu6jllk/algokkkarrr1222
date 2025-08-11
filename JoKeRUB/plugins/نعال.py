d1791ae60ed748119a99f97d94d8da123cef7907
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-11 04:48:48 -0700

    Update نعال.py

--- JoKeRUB/plugins/نعال.py
+++ JoKeRUB/plugins/نعال.py
@@ -23,3 +23,24 @@ async def send_naal(event):
             )
     except Exception as e:
         print("خطأ أثناء إرسال النعال:", e)
+
+NnAAL_STICKER = "هههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههه .متت"
+@l313l.on(events.NewMessage(pattern=r"^(?:\.|)ه$"))
+async def send_naal(event):
+    try:
+        await event.delete()  # حذف رسالتك الأصلية
+        
+        if event.is_reply:  
+            reply_msg = await event.get_reply_message()
+            await event.client.send_message(
+                event.chat_id,
+                NAAL_STICKER,
+                reply_to=reply_msg.id  # يرسل كرد على الرسالة اللي انت راد عليها
+            )
+        else:
+            await event.client.send_message(
+                event.chat_id,
+                NnAAL_STICKER  # يرسل بدون رد
+            )
+    except Exception as e:
+        print("خطأ أثناء إرسال النعال:", e)
