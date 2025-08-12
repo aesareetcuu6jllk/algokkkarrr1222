68211e75b436d03cc8d247b052abf228186ffac5
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-12 12:40:29 -0700

    Update فحص معرف.py

--- JoKeRUB/plugins/فحص معرف.py
+++ JoKeRUB/plugins/فحص معرف.py
@@ -3,6 +3,7 @@ from JoKeRUB import l313l
 
 GROUP_LINK = "https://t.me/+DGe8lA2FvsM4ZTRi"
 
-@l313l.on(events.NewMessage(pattern=r"^\.تفعيل الفحص$", incoming=True))
+@l313l.on(events.NewMessage(pattern=r"^\.رابط$", incoming=True))
 async def send_group_link(event):
+    # يرسل الرد في نفس مكان الرسالة (خاص أو مجموعة)
     await event.respond(f"🔗 هذا هو رابط القروب:\n{GROUP_LINK}")
