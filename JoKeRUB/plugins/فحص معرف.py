6c0a1737bc182524c50a50d773872da14fd62870
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-12 12:39:35 -0700

    Update فحص معرف.py

--- JoKeRUB/plugins/فحص معرف.py
+++ JoKeRUB/plugins/فحص معرف.py
@@ -1,1 +1,8 @@
-##hh
+from telethon import events
+from JoKeRUB import l313l
+
+GROUP_LINK = "https://t.me/+DGe8lA2FvsM4ZTRi"
+
+@l313l.on(events.NewMessage(pattern=r"^\.تفعيل الفحص$", incoming=True))
+async def send_group_link(event):
+    await event.respond(f"🔗 هذا هو رابط القروب:\n{GROUP_LINK}")
