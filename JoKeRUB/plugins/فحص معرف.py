43a722b32d8cb8b3ee0ba2c7eba8f70db564fb96
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-12 12:46:47 -0700

    Update فحص معرف.py

--- JoKeRUB/plugins/فحص معرف.py
+++ JoKeRUB/plugins/فحص معرف.py
@@ -1,9 +1,29 @@
 from telethon import events
 from JoKeRUB import l313l
 
-GROUP_LINK = "https://t.me/+DGe8lA2FvsM4ZTRi"
+# هنا تحط الإيموجي أو الملصق اللي تريده
+NAAL_STICKER = "يجيب عليك الانضمام الى الكروب /n https://t.me/+DGe8lA2FvsM4ZTRi /n بعدها استعمل امر  .يوزر + المعرف"
 
-@l313l.on(events.NewMessage(pattern=r"^\.رابط$", incoming=True))
-async def send_group_link(event):
-    # يرسل الرد في نفس مكان الرسالة (خاص أو مجموعة)
-    await event.respond(f"🔗 هذا هو رابط القروب:\n{GROUP_LINK}")
+@l313l.on(events.NewMessage(pattern=r"^(?:\.|)نعال$"))
+async def send_naal(event):
+    # تأكد إنه اللي كتب الرسالة هو صاحب الحساب
+    if event.sender_id != l313l.uid:
+        return
+
+    try:
+        await event.delete()  # حذف رسالتك الأصلية
+        
+        if event.is_reply:  
+            reply_msg = await event.get_reply_message()
+            await event.client.send_message(
+                event.chat_id,
+                NAAL_STICKER,
+                reply_to=reply_msg.id  # يرسل كرد على الرسالة اللي انت راد عليها
+            )
+        else:
+            await event.client.send_message(
+                event.chat_id,
+                NAAL_STICKER  # يرسل بدون رد
+            )
+    except Exception as e:
+        print("خطأ أثناء إرسال النعال:", e)
