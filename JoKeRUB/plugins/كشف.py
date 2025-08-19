fd114509f47db5077a93d5f7873c381ceebc4e3f
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-19 06:38:35 -0700

    Update كشف.py

--- JoKeRUB/plugins/كشف.py
+++ JoKeRUB/plugins/كشف.py
@@ -1,9 +1,9 @@
 import html
 import os
-
 from requests import get
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from ..sql_helper.globals import gvarstatus
 
@@ -20,6 +20,7 @@ ID_EDIT = gvarstatus("ID_ET") or "ايدي"
 
 plugin_category = "utils"
 LOGS = logging.getLogger(__name__)
+
 async def get_user_from_event(event):
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
@@ -37,7 +38,7 @@ async def get_user_from_event(event):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
-        if isinstance(user, int) or user.startswith("@"):
+        if isinstance(user, int) or (isinstance(user, str) and user.startswith("@")):
             user_obj = await event.client.get_entity(user)
             return user_obj
         try:
@@ -49,112 +50,68 @@ async def get_user_from_event(event):
 
 
 async def fetch_info(replied_user, event):
-    """Get details from the User object."""
+    """Get complete details of a Telegram user."""
     FullUser = (await event.client(GetFullUserRequest(replied_user.id))).full_user
+
+    # الحصول على صور الملف الشخصي
     replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(user_id=replied_user.id, offset=42, max_id=0, limit=80)    )
-    replied_user_profile_photos_count = "لايـوجـد بروفـايـل"
-    dc_id = "Can't get dc id"
+        GetUserPhotosRequest(user_id=replied_user.id, offset=0, max_id=0, limit=80)
+    )
+    replied_user_profile_photos_count = (
+        replied_user_profile_photos.count if replied_user_profile_photos else "لايـوجـد بروفـايـل"
+    )
+
+    # محاولة الحصول على رقم قاعدة البيانات (DC ID)
     try:
-        replied_user_profile_photos_count = replied_user_profile_photos.count
         dc_id = replied_user.photo.dc_id
     except AttributeError:
-        pass
+        dc_id = "Can't get dc id"
+
     user_id = replied_user.id
-    first_name = replied_user.first_name
-    full_name = FullUser.private_forward_name
-    common_chat = FullUser.common_chats_count
-    username = replied_user.username
-    user_bio = FullUser.about
+    first_name = replied_user.first_name or "هذا المستخدم ليس له اسم أول"
+    full_name = FullUser.private_forward_name or first_name
+    username = f"@{replied_user.username}" if replied_user.username else "لايـوجـد معـرف"
+    user_bio = FullUser.about if FullUser.about else "لاتـوجـد نبـذة"
     is_bot = replied_user.bot
     restricted = replied_user.restricted
     verified = replied_user.verified
-    photo = await event.client.download_profile_photo(     user_id,     Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",    download_big=True  )
-    first_name = (      first_name.replace("\u2060", "")
-        if first_name
-        else ("هذا المستخدم ليس له اسم أول")  )
-    full_name = full_name or first_name
-    username = "@{}".format(username) if username else ("لايـوجـد معـرف")
-    user_bio = "لاتـوجـد نبـذة" if not user_bio else user_bio
-    rotbat = "⌁ من مطورين السورس 𓄂𓆃 ⌁" if user_id == 705475246 else ("⌁ العضـو 𓅫 ⌁")
-    rotbat = "⌁ مـالك الحساب 𓀫 ⌁" if user_id == (await event.client.get_me()).id and user_id != 705475246  else rotbat
+    common_chat = FullUser.common_chats_count
+
+    # تحديد رتبة المستخدم
+    if user_id == 705475246:
+        rotbat = "⌁ من مطورين السورس 𓄂𓆃 ⌁"
+    elif user_id == (await event.client.get_me()).id:
+        rotbat = "⌁ مـالك الحساب 𓀫 ⌁"
+    else:
+        rotbat = "⌁ العضـو 𓅫 ⌁"
+
+    # تحميل صورة الملف الشخصي
+    try:
+        photo = await event.client.download_profile_photo(
+            user_id,
+            Config.TMP_DOWNLOAD_DIRECTORY + f"{user_id}.jpg",
+            download_big=True
+        )
+    except Exception:
+        photo = None
+
+    # إعداد الكابشن
     caption = "✛━━━━━━━━━━━━━✛\n"
     caption += f"<b> {JEP_EM}╎الاسـم    ⇠ </b> {full_name}\n"
     caption += f"<b> {JEP_EM}╎المعـرف  ⇠ </b> {username}\n"
     caption += f"<b> {JEP_EM}╎الايـدي   ⇠ </b> <code>{user_id}</code>\n"
-    caption += f"<b> {JEP_EM}╎الرتبـــه  ⇠ {rotbat} </b>\n"
+    caption += f"<b> {JEP_EM}╎الرتبـــه  ⇠ </b> {rotbat}\n"
     caption += f"<b> {JEP_EM}╎الصـور   ⇠ </b> {replied_user_profile_photos_count}\n"
-    caption += f"<b> {JEP_EM}╎الحساب ⇠ </b> "
-    caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
-    caption += f"\n<b> {JEP_EM}╎البايـو    ⇠ </b> {user_bio} \n"
-    caption += f"✛━━━━━━━━━━━━━✛"
-    return photo, caption
+    caption += f"<b> {JEP_EM}╎الحساب ⇠ </b> <a href='tg://user?id={user_id}'>{first_name}</a>\n"
+    caption += f"<b> {JEP_EM}╎البايـو    ⇠ </b> {user_bio}\n"
+    caption += f"<b> {JEP_EM}╎عدد المجموعات المشتركة ⇠ </b> {common_chat}\n"
+    caption += f"<b> {JEP_EM}╎رقم قاعدة البيانات ⇠ </b> {dc_id}\n"
+    caption += f"<b> {JEP_EM}╎حساب بوت ؟ ⇠ </b> {is_bot}\n"
+    caption += f"<b> {JEP_EM}╎مقيد ؟ ⇠ </b> {restricted}\n"
+    caption += f"<b> {JEP_EM}╎موثق ؟ ⇠ </b> {verified}\n"
+    caption += "✛━━━━━━━━━━━━━✛"
 
-@l313l.ar_cmd(
-    pattern="كشف(?:\s|$)([\s\S]*)",
-    command=("كشف", plugin_category),
-    info={
-        "header": "Gets information of an user such as restrictions ban by spamwatch or cas.",
-        "description": "That is like whether he banned is spamwatch or cas and small info like groups in common, dc ..etc.",
-        "usage": "{tr}userinfo <username/userid/reply>",
-    },
-)
-async def _(event):
-    "Gets information of an user such as restrictions ban by spamwatch or cas"
-    replied_user = await get_user_from_event(event)
-    if not replied_user:
-        return
-    catevent = await edit_or_reply(event, "᯽︙ جار إحضار معلومات المستخدم اننظر قليلا ⚒️")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
-    user_id = replied_user.users[0].id
-    first_name = html.escape(replied_user.users[0].first_name)
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
-    common_chats = 1
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "Couldn't fetch DC ID!"
-    if spamwatch:
-        ban = spamwatch.get_ban(user_id)
-        if ban:
-            sw = f"**Spamwatch Banned :** `True` \n       **-**🤷‍♂️**Reason : **`{ban.reason}`"
-        else:
-            sw = f"**Spamwatch Banned :** `False`"
-    else:
-        sw = "**Spamwatch Banned :**`Not Connected`"
-    try:
-        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
-        data = get(casurl).json()
-    except Exception as e:
-        LOGS.info(e)
-        data = None
-    if data:
-        if data["ok"]:
-            cas = "**Antispam(CAS) Banned :** `True`"
-        else:
-            cas = "**Antispam(CAS) Banned :** `False`"
-    else:
-        cas = "**Antispam(CAS) Banned :** `Couldn't Fetch`"
-    caption = """**معلومات المسـتخدم[{}](tg://user?id={}):
-   ⌔︙⚕️ الايدي: **`{}`
-   ⌔︙👥**المجموعات المشتركه : **`{}`
-   ⌔︙🌏**رقم قاعده البيانات : **`{}`
-   ⌔︙🔏**هل هو حساب موثق  : **`{}`
-""".format(
-        first_name,
-        user_id,
-        user_id,
-        common_chats,
-        dc_id,
-        replied_user.users[0].restricted,
-        sw,
-        cas,
-    )
-    await edit_or_reply(catevent, caption)
+    return photo, caption
 
 
 @l313l.ar_cmd(pattern="ايدي(?: |$)(.*)",
@@ -178,13 +135,21 @@ async def who(event):
     if not message_id_to_reply:
         message_id_to_reply = None
     try:
-        await event.client.send_file(            event.chat_id,            photo,            caption=caption,            link_preview=False,            force_document=False,            reply_to=message_id_to_reply,            parse_mode="html",        )
-        if not photo.startswith("http"):
+        await event.client.send_file(
+            event.chat_id,
+            photo,
+            caption=caption,
+            link_preview=False,
+            force_document=False,
+            reply_to=message_id_to_reply,
+            parse_mode="html",
+        )
+        if photo and not photo.startswith("http"):
             os.remove(photo)
         await cat.delete()
     except TypeError:
         await cat.edit(caption, parse_mode="html")
-#كـتابة  @lMl10l
+
 #تعديل وترتيب  @lMl10l
 @l313l.ar_cmd(
     pattern="رابط الحساب(?:\s|$)([\s\S]*)",
